7013143d8d6c1e4219298b0848c6f016f339d321
Santiago <sammysan007@gmail.com>
2022-05-18 21:35:56 -0300

    added morgan

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
+const morgan = require('morgan')
 const app = express();
 
 const ocurrencesRoutes = require('./routes/ocurrences.routes');
 const countOcurrencesRoutes = require('./routes/count-ocurrences.routes');
 
 //#endregion
-
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 app.use('/ocurrences', ocurrencesRoutes);
 app.use('/count-ocurrences', countOcurrencesRoutes);
@@ -24,5 +21,10 @@ const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
 const url = `mongodb+srv://${ DB_USER }:${ DB_PASSWORD }@apicluster.y3xzl.mongodb.net/andonDb?retryWrites=true&w=majority`;
 
 mongoose.connect(url)
-  .then(() => app.listen(process.env.PORT || 3000))
-  .catch((err) => console.log(err))
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.listen(process.env.PORT || 3000)
